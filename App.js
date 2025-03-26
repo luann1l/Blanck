db6fbca45873794add96836ce17b87b1bcdbe976
luann <lv483713@gmail.com>
2025-03-26 13:08:08 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <h1>Idol Kpop</h1>
+
+        <img
+          src="https://pbs.twimg.com/media/FQ-cc3BVEAE28gG.jpg"
+          alt="Minha foto"
+          className="profile-img"
+        />
+
+        <p className="sobre">
+        Qual queen Do floptok Você seria ? 💋
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
